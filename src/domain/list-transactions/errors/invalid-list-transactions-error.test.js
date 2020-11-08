76d08c8479b22048f468d9cf99b3291d7688b627
Ozeas Souza <computer.ozeas@gmail.com>
2020-11-07 22:26:43 -0300

    test: list transactions errors

---
+++ src/domain/list-transactions/errors/invalid-list-transactions-error.test.js
@@ -0,0 +1,19 @@
+import InvalidListTransactionsError from './invalid-list-transactions-error';
+
+const makeError = (code = 400) => () => {
+  throw new InvalidListTransactionsError(code);
+};
+
+describe('InvalidListTransactionsError', () => {
+  it('should throw notfound_error as default', () => {
+    expect(makeError()).toThrow('notfound_error');
+  });
+
+  test.each([
+    [400, 'notfound_error'],
+    [500, 'internal_error'],
+    [408, 'timeout_error']
+  ])('%#: should %s throw %s exception', (codeError, typeError) => {
+    expect(makeError(codeError)).toThrow(typeError);
+  });
+});
