c7f728e8d2261a31807d3247ebbd7a21e48eacc6
Ozeas Souza <computer.ozeas@gmail.com>
2020-11-07 22:45:00 -0300

    test: list transactions case

---
+++ src/domain/list-transactions/list-transactions.test.js
@@ -0,0 +1,14 @@
+import ListTransactions from './list-transactions';
+
+describe('ListTransactions', () => {
+  const request = {
+    getList: jest.fn()
+  };
+
+  it('should call getList requests method', async () => {
+    const instance = new ListTransactions(request);
+    await instance.getList();
+
+    expect(request.getList).toHaveBeenCalled();
+  });
+});
