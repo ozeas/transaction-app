ed62a3936642afe5ec5ed264c1539d3fe44c49bc
Ozeas Souza <computer.ozeas@gmail.com>
2020-11-11 14:51:41 -0300

    refactor: add config theme styled

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,17 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import { theme, GlobalStyles } from '../src/presentation/style-tokens';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  ),
+];
