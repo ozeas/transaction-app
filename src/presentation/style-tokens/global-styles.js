06c6186b1d5081d0585369868e0de2bb44300ea7
Ozeas Souza <computer.ozeas@gmail.com>
2020-11-17 20:36:22 -0300

    refactor: fix load font-family

--- src/presentation/style-tokens/global-styles.js
+++ src/presentation/style-tokens/global-styles.js
@@ -3,7 +3,6 @@ import { createGlobalStyle } from 'styled-components';
 import theme from './theme';
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
   * {
     font-family: ${theme.font};
     font-size: ${theme.fontSizes[1]}px;
