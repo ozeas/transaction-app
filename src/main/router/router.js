4e56a21ad83073d40e100532d79bcb0004339cb8
Ozeas Souza <computer.ozeas@gmail.com>
2020-11-17 23:42:05 -0300

    feat: add Recoil Router

--- src/main/router/router.js
+++ src/main/router/router.js
@@ -1,17 +1,20 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import React from 'react';
+import { RecoilRoot } from 'recoil';
 
 import { makeCreate, makeList } from '@main/pages';
 
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={makeList} />
-        <Route path="/list" exact component={makeList} />
-        <Route path="/create" exact component={makeCreate} />
-      </Switch>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={makeList} />
+          <Route path="/list" exact component={makeList} />
+          <Route path="/create" exact component={makeCreate} />
+        </Switch>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 };
 
