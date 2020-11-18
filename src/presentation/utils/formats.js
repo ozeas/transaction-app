dbb27e3b93c386e0c658932982fc8d208dc7a613
Ozeas Souza <computer.ozeas@gmail.com>
2020-11-18 15:48:38 -0300

    feat: add date format

--- src/presentation/utils/formats.js
+++ src/presentation/utils/formats.js
@@ -10,3 +10,15 @@ export const currencyFormat = (
 
 export const numberFormat = (value, countryLanguage = 'pt-BR') =>
   new Intl.NumberFormat(countryLanguage, { style: 'decimal' }).format(value);
+
+export const dateTimeFormat = (value, countryLanguage = 'pt-BR') => {
+  const options1 = {
+    hour: 'numeric',
+    minute: 'numeric',
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+  };
+  const date = value ? new Date(value) : new Date();
+  return new Intl.DateTimeFormat(countryLanguage, options1).format(date);
+};
