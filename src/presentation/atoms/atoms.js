2fc1f3bed20263e2221b19a233acae6dc09b6d8e
Ozeas Souza <computer.ozeas@gmail.com>
2020-11-18 09:23:41 -0300

    feat: add state to warning message

--- src/presentation/atoms/atoms.js
+++ src/presentation/atoms/atoms.js
@@ -4,3 +4,11 @@ export const transactionListAtom = atom({
   key: 'transactionListAtom',
   default: [],
 });
+
+export const warningApplicationAtom = atom({
+  key: 'warningApplicationAtom',
+  default: {
+    enable: false,
+    message: '',
+  },
+});
